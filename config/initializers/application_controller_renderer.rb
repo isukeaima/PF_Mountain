45536c04d56ab3f0d63136e82c4d6e577a801c6c
isukeaima <asaumi.kei49@gmial.com>
2021-08-10 08:40:25 +0000

    F

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd7ce20d577fdc27405b936404d76ad60ac97fcb90cc136987d7e25b857f8c42d1bb4aa56b5a1c52683b49c76d1515c8bef9bc246691bb1ae993c3575d3ae7999'
