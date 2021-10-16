d4bbfc46343a7e89c5a02b26595bbd2d3fe5459f
isukeaima <asaumi.kei49@gmail.com>
2021-10-16 07:17:32 +0000

    ユーザー一覧追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   get 'users/show'
   root to:'homes#top'
-  
+
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show,:edit,:update]do
+  resources :users, only: [:index, :show,:edit,:update]do
     resource :relationships, only: [:create, :destroy]
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
+
 end
